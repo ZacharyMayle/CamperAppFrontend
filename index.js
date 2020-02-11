6734ff1a8cc30555a073ad0107d8bb79c92f8d81
ZacharyMayle <zam1998@gmail.com>
2020-02-11 15:51:23 -0700

    Fixed the frontend issues, adding the hamburger again.

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
 const userList = document.getElementById("list");
-
 fetch("http://localhost:3001/states.json")
   .then(response => response.json())
   .then(states =>
     Object.keys(states).map(state => {
       let option = document.createElement("option");
       let select = document.querySelector("select");
-
       option.value = state;
       option.innerText = state;
       select.appendChild(option);
     })
   );
-
 fetch("http://localhost:3000/users")
   .then(response => response.json())
   .then(users => {
+    // console.log(users)
     users.map(user => {
       let li = document.createElement("li");
       li.innerHTML = `${user.first_name} ${user.last_name}`;
       userList.append(li);
     });
   });
-
 document.getElementById("existingUser").addEventListener("click", function() {
   let list = document.getElementById("modalBox");
   list.style.visibility =
     list.style.visibility == "visible" ? "hidden" : "visible";
 });
-
 document.getElementById("newUser").addEventListener("click", function() {
   let form = document.getElementById("modalForm");
   form.style.visibility =
     form.style.visibility == "visible" ? "hidden" : "visible";
 });
+
 
 //            Controls for the hamburger
 function myFunction(x) {
