72073af93d745d5922c7a77b463f76fa771adc92
github username <github email address>
2020-02-11 11:55:53 -0700

    Added US states JSON file as well as US 2-ch abbrev to list for adding new User

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 userList = document.getElementById("list");
 
+fetch("http://localhost:3000/states.json")
+  .then(response => response.json())
+  // .then(response => console.log(response))
+  .then(states =>
+    Object.keys(states).map(state => {
+      let option = document.createElement("option");
+      let select = document.querySelector("select");
+
+      option.value = state;
+      option.innerText = state;
+      select.appendChild(option);
+    })
+  );
+
 fetch("http://localhost:3002/users")
   .then(response => response.json())
   .then(users => {
     users.map(user => {
       let li = document.createElement("li");
       li.innerHTML = `${user.first_name} ${user.last_name}`;
-      userList.append(li)
+      userList.append(li);
     });
   });
 
