63ac893f2022887a083e7233c9c6ce7a403f0a8e
github username <github email address>
2020-02-11 10:27:41 -0700

    Added modal form and modal list of users from database

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 fetch("http://localhost:3002/users")
   .then(response => response.json())
   .then(users => {
-    users.map(user =>
-      document
-        .getElementById("existingUser")
-        .addEventListener("click", function() {
-          document.getElementById(
-            "modalBox"
-          ).innerHTML = `${user.first_name} ${user.last_name}`;
-        })
-    );
+    users.map(user => {
+      let box = document.getElementById("modalBox");
+      box.innerHTML = `${user.first_name} ${user.last_name}`;
+    });
   });
+
+document.getElementById("existingUser").addEventListener("click", function() {
+  let list = document.getElementById("modalBox");
+  list.style.visibility =
+    list.style.visibility == "visible" ? "hidden" : "visible";
+});
+
+document.getElementById("newUser").addEventListener("click", function() {
+  let form = document.getElementById("modalForm");
+  form.style.visibility =
+    form.style.visibility == "visible" ? "hidden" : "visible";
+});
