8ebfab9f5144bfbe6a4d1c8d4c5e06b34863fc76
github username <github email address>
2020-02-11 09:53:25 -0700

    Added existing user button

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hit me!")
+fetch("http://localhost:3002/users")
+  .then(response => response.json())
+  .then(users => {
+    users.map(user =>
+      document
+        .getElementById("existingUser")
+        .addEventListener("click", function() {
+          document.getElementById(
+            "modalBox"
+          ).innerHTML = `${user.first_name} ${user.last_name}`;
+        })
+    );
+  });
