fc73422d2618ae453aa9bc22e734b09b50280e86
ZacharyMayle <zam1998@gmail.com>
2020-02-14 10:23:34 -0700

    Finished adding content to the camp info page, changed the CSS for the map and reservations section

--- campground-info-page.js
+++ campground-info-page.js
@@ -51,19 +51,67 @@ fetch(`http://localhost:3000/campgrounds/${query}`)
 
 
   //            Wheel Chair Access 
-
+  fetch(`http://localhost:3000/campgrounds/${query}`)
+  .then(response => response.json())
+  .then(campground => {
+    console.log(campground);
+    let li = document.createElement("li");
+    li.innerHTML = `<h3 id="wheel-chair-name">Wheel Chair Access</h3> <p class="camp-description">${campground.wheel_chair}</p>`;
+    li.className = "wheel-chair-list"
+    campgroundInfo.prepend(li);
+  });
 
   //            Cell Phone
-
+  fetch(`http://localhost:3000/campgrounds/${query}`)
+  .then(response => response.json())
+  .then(campground => {
+    console.log(campground);
+    let li = document.createElement("li");
+    li.innerHTML = `<h3 id="cell-phone-name">Cell Reception</h3> <p class="camp-description">${campground.cell_phone}</p>`;
+    li.className = "cell-phone-list"
+    campgroundInfo.prepend(li);
+  });
 
   //            Fire Stove
-
+  fetch(`http://localhost:3000/campgrounds/${query}`)
+  .then(response => response.json())
+  .then(campground => {
+    console.log(campground);
+    let li = document.createElement("li");
+    li.innerHTML = `<h3 id="fire-stove-name">Fire Stove Policy</h3> <p class="camp-description">${campground.fire_stove}</p>`;
+    li.className = "fire-stove-list"
+    campgroundInfo.prepend(li);
+  });
 
   //            Toilets
-
+  fetch(`http://localhost:3000/campgrounds/${query}`)
+  .then(response => response.json())
+  .then(campground => {
+    console.log(campground);
+    let li = document.createElement("li");
+    li.innerHTML = `<h3 id="toilets-name">Toilet Information</h3> <p class="camp-description">${campground.toilets}</p>`;
+    li.className = "toilet-list"
+    campgroundInfo.prepend(li);
+  });
 
   //            Showers
-
+  fetch(`http://localhost:3000/campgrounds/${query}`)
+  .then(response => response.json())
+  .then(campground => {
+    console.log(campground);
+    let li = document.createElement("li");
+    li.innerHTML = `<h3 id="showers-name">Shower Information</h3> <p class="camp-description">${campground.showers}</p>`;
+    li.className = "showers-list"
+    campgroundInfo.prepend(li);
+  });
 
   //            Laundry
-
+  fetch(`http://localhost:3000/campgrounds/${query}`)
+  .then(response => response.json())
+  .then(campground => {
+    console.log(campground);
+    let li = document.createElement("li");
+    li.innerHTML = `<h3 id="laundry-name">Is Laundry Available?</h3> <p class="camp-description">${campground.laundry}</p>`;
+    li.className = "laundry-list"
+    campgroundInfo.prepend(li);
+  });
