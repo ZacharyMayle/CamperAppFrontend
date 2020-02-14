82e09b68d175916bfa9000e32cb4786ba7c2f6c7
github username <github email address>
2020-02-14 13:19:38 -0700

    added logic to parks with camps page to change message if a park doesn't have any campgrounds

--- parks-with-camps.js
+++ parks-with-camps.js
@@ -28,17 +28,14 @@ fetch(`http://localhost:3000/parks/${query_id}`)
     console.log(park);
     let h2 = document.createElement("h2");
     let p = document.createElement("p");
-    // console.log(park.camgrounds != undefined)
     h2.innerHTML = `${park.name} - ${park.designation} <p class="camp-description">${park.description}</p>`;
     console.log(park.campgrounds.length != []);
     if (park.campgrounds.length != []) {
       p.innerText = "Select from the list of Campgrounds below: ";
       park.campgrounds.map(campground => {
-        console.log(campground)
         let li = document.createElement("li");
         li.innerHTML = `<a href="http://localhost:3001/campground-info-page.html?id=${campground.id}">${campground.name}</a>`;
         p.append(li)
-        // console.log("hit")
       });
     } else if (park.campgrounds.length == []) {
       p.innerText = `This park does not contain any campgrounds at this time. Please check back for updates!`;
